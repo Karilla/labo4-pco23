f90a51ae63ffd27f333fc06777291160a55a0d8d
Karilla <45608439+Karilla@users.noreply.github.com>
2023-11-16 19:28:36 +0100

    Update locomotivebehavior.cpp

--- code/src/locomotivebehavior.cpp
+++ code/src/locomotivebehavior.cpp
@@ -9,6 +9,8 @@
 #include "locomotivebehavior.h"
 #include "ctrain_handler.h"
 
+using LocoID = int;
+
 void LocomotiveBehavior::run()
 {
     //Initialisation de la locomotive
@@ -17,6 +19,9 @@ void LocomotiveBehavior::run()
     loco.afficherMessage("Ready!");
 
     /* A vous de jouer ! */
+    unsigned int nbTourLocoA = 0;
+    unsigned int nbTourLocoB = 0;
+    LocoID locoId = 0;
 
     // Vous pouvez appeler les méthodes de la section partagée comme ceci :
     //sharedSection->access(loco);
@@ -24,10 +29,49 @@ void LocomotiveBehavior::run()
     //sharedSection->stopAtStation(loco);
 
     while(true) {
+        //Section critique 25 22
+        // 10 14
         // On attend qu'une locomotive arrive sur le contact 1.
         // Pertinent de faire ça dans les deux threads? Pas sûr...
-        attendre_contact(31);
-        attendre_contact(30);
+        //attendre_contact(25);
+        if((loco.numero() == 0) ){
+            attendre_contact(29);
+            sharedSection->stopAtStation(loco);
+            nbTourLocoA++;
+            locoId = loco.numero();
+            if(nbTourLocoA <= nbTourLocoB && locoId !=/* On sait pas encore */ loco.numero()){
+              loco.priority = 1;
+              }
+            else{
+                loco.priority = 0;
+              }
+            attendre_contact(22);
+            loco.afficherMessage("J'arrive dans la section critique");
+            sharedSection->access(loco);
+            attendre_contact(15);
+            diriger_aiguillage(8,TOUT_DROIT,0);
+            attendre_contact(10);
+            sharedSection->leave(loco);
+          }
+        else{
+            attendre_contact(33);
+            sharedSection->stopAtStation(loco);
+            nbTourLocoB++;
+            locoId = loco.numero();
+            if(nbTourLocoA <= nbTourLocoB && locoId !=/* On sait pas encore */ loco.numero()){
+              loco.priority;
+              }
+            else{
+                loco.priority = 0;
+              }
+            attendre_contact(25);
+            loco.afficherMessage("J'arrive en section critique");
+            sharedSection->access(loco);
+            attendre_contact(15);
+            diriger_aiguillage(8,DEVIE,0);
+            attendre_contact(14);
+            sharedSection->leave(loco);
+          }
         loco.afficherMessage("J'ai atteint le contact 1");
     }
 }
